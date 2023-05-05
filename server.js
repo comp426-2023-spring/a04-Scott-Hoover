bf681989bb43c991b8f17cd05324ac885a9c60ab
Scott Hoover <scott@LAPTOP-8EVT7IUT>
2023-05-04 22:34:55 -0400

    layout

--- server.js
+++ server.js
@@ -4,4 +4,33 @@ import minimist from minimist
 
 
 const app = express();
+//2
 const port = args.port || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+//req 3 /app
+app.get(app, (req, res) => {
+	status(200).send(200 OK);
+}
+//404 not found
+
+// /app/rps
+
+
+///app/rpsls/
+
+
+///app/rps/play/
+
+
+///app/rpsls/play/
+
+
+// /app/rpsls/play/(rock|paper|scissors)/
+
+
+// /app/rpsls/play/(rock|paper|scissors|lizard|spock)/
+
+
+
