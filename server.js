fa65d4df56fcd9abb1e2731213b710f320e15890
Scott Hoover <scott@LAPTOP-8EVT7IUT>
2023-05-05 13:45:13 -0400

    fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import {rps, rpsls from ./lib/rpsls.js};
-import express from express;
-import minimist from minimist;
+import express from "express";
+import minimist from "minimist";
 
 
 const app = express();
